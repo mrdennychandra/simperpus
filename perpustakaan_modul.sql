32c4a1bddfd849eb677cc3fe9315677ca66743c5
mrdennychandra <dennyc.dev@gmail.com>
2018-11-22 12:18:31 +0700

    db

---
+++ perpustakaan_modul.sql
@@ -0,0 +1,58 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.32-MariaDB : Database - perpustakaan
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`perpustakaan` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `perpustakaan`;
+
+/*Table structure for table `buku` */
+
+DROP TABLE IF EXISTS `buku`;
+
+CREATE TABLE `buku` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `isbn` varchar(15) DEFAULT NULL,
+  `judul` varchar(50) DEFAULT NULL,
+  `idpengarang` int(50) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pegarang` (`idpengarang`),
+  CONSTRAINT `fk_pegarang` FOREIGN KEY (`idpengarang`) REFERENCES `pengarang` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+/*Data for the table `buku` */
+
+insert  into `buku`(`id`,`isbn`,`judul`,`idpengarang`,`stok`,`gambar`) values (5,'123456','Coding PHP',1,1000,'1logo.png');
+insert  into `buku`(`id`,`isbn`,`judul`,`idpengarang`,`stok`,`gambar`) values (8,'12333','Coding Java',1,220,'1logo.png');
+
+/*Table structure for table `pengarang` */
+
+DROP TABLE IF EXISTS `pengarang`;
+
+CREATE TABLE `pengarang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `pengarang` */
+
+insert  into `pengarang`(`id`,`nama`,`email`) values (1,'Denny','chandra@gmail.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
